8ea3cc62c262ea4a532fb883c93cc427cc541911
AjitJadhav8 <ajitjadhav30april@gmail.com>
2024-12-03 09:58:51 +0530

    Demo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'demo',
+  password: 'Pass@123',
+  database: 'recruitment_db_main',
 });
 
 db.getConnection((err) => {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000'
-    // apiUrl: 'http://3.6.117.163:3000'
+    apiUrl: 'http://139.59.22.62:3000'
+
+    // apiUrl: 'http://localhost:3000'
 
 
-      // apiUrl = 'http://3.6.117.163:3000'; // Base URL for your API
 
   };
